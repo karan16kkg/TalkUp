3a883ab01edaca956ef462c3474dc427ebb08b63
karan16kkg <karankumargarg1610@gmail.com>
2025-02-24 23:51:21 +0530

    Do changes in notification

--- Frontend/src/components/Home/Notification.jsx
+++ Frontend/src/components/Home/Notification.jsx
@@ -3,27 +3,35 @@ import { ChatState } from '../../Context/ChatProvider'
 import { getSender } from '../../config/ChatLogics';
 
 const Notification = ({ onClose }) => {
-    const { user, notification, setnotification, selectedChat,setselectedChat } = ChatState();
+    const { user, notification, setnotification, selectedChat, setselectedChat } = ChatState();
     return (
         <div className='fixed inset-0 flex bg-opacity-0 z-50'>
             <div className='style absolute w-full md:w-1/3 lg:w-1/3 h-screen bg-teal-50'>
                 <button className="absolute top-1 right-3 text-3xl text-gray-600 hover:text-gray-800" onClick={onClose}> ✕ </button>
 
                 <div className='mt-14 px-4'>
-                    {notification.length === 0}{
+                    {notification.length === 0 ? (
                         <div className='text-3xl flex justify-center items-center'>No new Notification</div>
-                    }
-                    {notification && notification.map((user1, index) => (
+                    ) : (
                         <>
-                        <div key={index} onClick={()=>{setselectedChat(user1.chat),setnotification(notification.filter((n) => n!=user1))}} className='h-14 mt-4 bg-teal-200 px-3 flex items-center text-2xl cursor-pointer'>
-                            {user1.chat.isGroupChat ? `New Messsage in:  ${user1.chat.ChatName}` : `New Messsage from:  ${getSender(user, user1.chat.users)}`}
-                        </div>
-                        <div className='h-14 mt-4 bg-teal-200 px-3 flex items-center text-2xl cursor-pointer'>
-                            hello
-                        </div>
+                            <h2 className='text-4xl font-bold text-center mb-4'>Notifications</h2>
+                            {notification.map((user1, index) => (
+                                <div key={index}
+                                    onClick={() => {
+                                        setselectedChat(user1.chat);
+                                        setnotification(notification.filter((n) => n !== user1));
+                                    }}
+                                    className='h-14 mt-4 bg-teal-200 px-3 flex items-center text-2xl cursor-pointer'
+                                >
+                                    {user1.chat.isGroupChat
+                                        ? `New Message in: ${user1.chat.ChatName}`
+                                        : `New Message from: ${getSender(user, user1.chat.users)}`}
+                                </div>
+                            ))}
                         </>
-                    ))}
+                    )}
                 </div>
+
             </div>
         </div>
     )
